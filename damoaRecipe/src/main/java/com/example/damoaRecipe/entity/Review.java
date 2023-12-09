02b43d0caef970a7dbe9aaa80d29c9f18c80620f
HyunsooKim <hyunsu142@naver.com>
2023-12-09 19:16:55 +0900

    Delete damoaRecipe directory

--- damoaRecipe/src/main/java/com/example/damoaRecipe/entity/Review.java
+++
@@ -1,4 +0,0 @@
-package com.example.damoaRecipe.entity;
-
-public class Review {
-}
--- damoaRecipe/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Categories ("한식")  values ('category 1');
-INSERT INTO Categories ("중식")  values ('category 2');
-INSERT INTO Categories ("일식")  values ('category 3');
-
