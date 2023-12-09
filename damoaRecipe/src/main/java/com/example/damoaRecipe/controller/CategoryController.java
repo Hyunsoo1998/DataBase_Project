f4d483b508f32f12ec3e40381d5825220e8d7a3f
HyunsooKim <hyunsu142@naver.com>
2023-12-09 17:33:39 +0900

    Update CategoryController.java

--- damoaRecipe/src/main/java/com/example/damoaRecipe/controller/CategoryController.java
+++ damoaRecipe/src/main/java/com/example/damoaRecipe/controller/CategoryController.java
@@ -3,23 +3,35 @@ package com.example.damoaRecipe.controller;
 
 import com.example.damoaRecipe.entity.Category;
 import com.example.damoaRecipe.exception.DataNotFoundException;
+import com.example.damoaRecipe.repository.CategoryRepository;
 import com.example.damoaRecipe.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
+
     private final CategoryService categoryService;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
     // 카테고리 조회 API
     @GetMapping("/{categoryId}")
     public Category getCategoryById(@PathVariable Long categoryId) {
@@ -27,7 +39,15 @@ public class CategoryController {
         return categoryOptional.orElseThrow(() -> new DataNotFoundException("Category not found with ID: " + categoryId));
     }
 
-    // 다양한 카테고리 관련 API를 추가할 수 있습니다.
+    //  API를 추가부분
 
+    
+
+
+    // 예외 처리 추가
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<String> handleDataNotFoundException(DataNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
 
